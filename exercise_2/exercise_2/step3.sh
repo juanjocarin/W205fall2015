9ff2167b51830c141e2f2758fe5eeea6e68dea64
Juanjo Carin <juanjose.carin@ischool.berkeley.edu>
2015-12-04 19:55:37 +0000

    Cleaning

--- exercise_2/exercise_2/step3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Initialize Postgres
-/data/start_postgres.sh
-#sudo -u postgres psql
-#Create DB
-python ./createDB.py
-## AFTER changing the spout and bolts
-cd ./EX2Tweetwordcount
-sparse run
